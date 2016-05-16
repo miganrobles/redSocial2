5e27f3a4f08983a5cf424f017db15a59910292af
miguel <miganrobles@gmail.com>
2016-05-16 13:08:45 +0200

    Eliminada repetición de código de la clase NewsFeed

--- NewsFeed.java
+++ NewsFeed.java
@@ -8,19 +8,14 @@ import java.util.ArrayList;
  */
 public class NewsFeed
 {
-    private ArrayList<MessagePost> messages;
-    private ArrayList<PhotoPost> photos;
-    private ArrayList<JoinGroupPost> grupoPosts;
+    private ArrayList<Post> posts;
 
     /**
      * Constructor for objects of class NewsFeed
      */
     public NewsFeed()
     {
-        messages = new ArrayList<>();
-        photos = new ArrayList<>();
-        grupoPosts = new ArrayList<>();
-        
+        posts = new ArrayList<>();
     }
 
     /**
@@ -28,43 +23,17 @@ public class NewsFeed
      * 
      * @message the message to introduce
      */
-    public void addMessagePost(MessagePost message)
+    public void addPost(Post post)
     {
-        messages.add(message);
-    }
-
-    /**
-     * Add a post with a image
-     * 
-     * @message the image to introduce
-     */
-    public void addPhotoPost(PhotoPost photo)
-    {
-        photos.add(photo);
-    }
-    
-    /**
-     * Add a post with a message
-     * 
-     * @message the message to introduce
-     */
-    public void addGrupoPost(JoinGroupPost grupoPost)
-    {
-        grupoPosts.add(grupoPost);
+        posts.add(post);
     }
     
     /**
      * Show the post
      */
     public void show(){
-        for(MessagePost message : messages){
-            message.display();
-        }
-        for(PhotoPost photo : photos){
-            photo.display();
-        }
-        for(JoinGroupPost grupoPost : grupoPosts){
-            grupoPost.display();
-        }
+        for(Post post : posts){
+            post.display();
+        }  
     }
 }
